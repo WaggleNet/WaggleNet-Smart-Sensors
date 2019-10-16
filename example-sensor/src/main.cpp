e2646490c2cb58535796653cfa8dcdb6a20ec72b
Streamliner <jimmy.xrail@me.com>
2019-10-15 20:01:42 -0500

    Sensor library is now linked in

---
+++ example-sensor/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#define UNO
+#include "pnpsensor.h"
+
+PnPSensor sensor;
+
+int analogReading = 0;
+float constant = 42;
+uint32_t currTime = millis()/1000;
+
+void getAnalogReading() {
+  analogReading = analogRead(A0);
+}
+
+void getCurrTime() {
+  currTime = millis() / 1000;
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Sensor is setting up");
+  sensor.type = 0x1001L;
+  sensor.addAutoEntry(analogReading, getAnalogReading);
+  sensor.addAutoEntry(currTime, getCurrTime);
+  sensor.addEntry(constant);
+  Serial.println("Sensor has started");
+  sensor.begin(0x23);
+  StartSensor(sensor);
+}
+
+void loop() {
+  sensor.update();
+}
