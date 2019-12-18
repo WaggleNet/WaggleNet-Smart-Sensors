5206e3ba424003afb062d54ae4b8f9e8f555f517
Jimmy He <jimmy.xrail@me.com>
2019-12-18 15:24:19 -0600

    Vibration sensor merry christmas

--- vib-light-sensor/src/main.cpp
+++ vib-light-sensor/src/main.cpp
@@ -1,20 +1,24 @@
 #include <Arduino.h>
 #include <Wire.h>
-#define UNO
+//#define UNO
 #include "pnpsensor.h"
 
-#define SENSOR_TYPE 0x1002
+#define SENSOR_TYPE 1002
 #define LED_INDICATOR 9
-#define AMLIGHT A3
-#define VIBSW A2
+#define AMLIGHT A2
+#define VIBSW A3
 
 PnPSensor sensor;
 
 uint32_t ambient_light;
 uint8_t hasVibration = 0;
 
+//new stuff
+uint8_t vib_check = 0;
+uint8_t reading_size = 100;
+
 void get_ambient_light() {
-  ambient_light = analogRead(AMLIGHT);
+  ambient_light = 1023-analogRead(AMLIGHT);
 }
 
 void setup() {
@@ -35,10 +39,23 @@ void setup() {
   Serial.println("-!>\tOK\tSystem");
 }
 
+//new stuff
+int get_vibration(){
+  vib_check = 0;
+  for(int i = 0; i < reading_size; i++){
+      vib_check += digitalRead(VIBSW); 
+      delay(1);
+  }
+  if(vib_check > 1){
+      return 1;
+  }
+  else{
+    return 0;
+  } 
+}
+
 void loop() {
   sensor.update();
-  if (!digitalRead(VIBSW)) {
-    hasVibration = 1;
-    sensor.changed(1);
-  }
+  hasVibration = get_vibration();
+  sensor.changed(1);
 }
