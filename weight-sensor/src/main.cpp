98c369c47f9a67a74d4df4501b74eee83f65289b
Sebastian Moruszewicz <sebasmorus@gmail.com>
2020-02-22 19:35:46 -0600

    Sensor Firmware Pending Testing

--- weight-sensor/src/main.cpp
+++ weight-sensor/src/main.cpp
@@ -4,59 +4,37 @@
 #include "pnpsensor.h"
 #include "HX711.h"
 
-#define SENSOR_TYPE 1002
-#define LED_INDICATOR 9
-#define AMLIGHT A2
-#define VIBSW A3
+#define SENSOR_TYPE 1004
+#define LED_PIN 9
+#define HX711CLK A1
+#define HX711DATA A2
 
 PnPSensor sensor;
+HX711 scale;
 
-uint32_t ambient_light;
-uint8_t hasVibration = 0;
+uint32_t weight;
 
-//new stuff
-uint8_t vib_check = 0;
-uint8_t reading_size = 100;
-
-void get_ambient_light() {
-  ambient_light = 1023-analogRead(AMLIGHT);
+void get_weight_reading(){
+  weight = scale.read_average(uint8_t(100));
 }
 
 void setup() {
-  pinMode(LED_INDICATOR, OUTPUT);
-  digitalWrite(LED_INDICATOR, HIGH);
-  pinMode(VIBSW, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
   Serial.begin(115200);
   Serial.println("-!>\tSTART\tSystem");
   sensor.type = SENSOR_TYPE;
-  sensor.addAutoUintEntry(ambient_light, get_ambient_light);
-  sensor.addUintEntry(hasVibration);
+  sensor.addAutoUintEntry(weight, get_weight_reading);
   uint8_t address = readAddress();
   Serial.print("-->\tSystem.Address\t0x");
   Serial.println(address, HEX);
   sensor.begin(address);
   StartSensor(sensor);
-  digitalWrite(LED_INDICATOR, LOW);
+  digitalWrite(LED_PIN, LOW);
   Serial.println("-!>\tOK\tSystem");
-}
-
-//new stuff
-int get_vibration(){
-  vib_check = 0;
-  for(int i = 0; i < reading_size; i++){
-      vib_check += digitalRead(VIBSW); 
-      delay(1);
-  }
-  if(vib_check > 1){
-      return 1;
-  }
-  else{
-    return 0;
-  } 
 }
 
 void loop() {
   sensor.update();
-  hasVibration = get_vibration();
   sensor.changed(1);
 }
