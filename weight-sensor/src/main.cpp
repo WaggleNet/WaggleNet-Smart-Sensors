05707dc0ba885f81bcf8574093a8e54126e713ee
Cameron Fuller <Cameron@wirelessprv-10-193-103-116.near.illinois.edu>
2020-02-27 20:56:07 -0600

    fixed main.cpp, added Uno env

--- weight-sensor/src/main.cpp
+++ weight-sensor/src/main.cpp
@@ -6,8 +6,8 @@
 
 #define SENSOR_TYPE 1004
 #define LED_PIN 9
-#define HX711CLK A1
-#define HX711DATA A2
+#define HX711CLK A2
+#define HX711DATA A1
 
 PnPSensor sensor;
 HX711 scale;
@@ -15,16 +15,30 @@ HX711 scale;
 uint32_t weight;
 
 void get_weight_reading(){
+  //digitalWrite(LED_PIN, HIGH);
+  //Serial.println("starting");
   weight = scale.read_average(uint8_t(100));
+  // Serial.println(weight);
+  // Serial.println("done");
+  //digitalWrite(LED_PIN, LOW);
+  //weight = 0;
 }
+
+// uint32_t bla;
+// void get_blablabla(){
+//   bla = millis();
+// }
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
   Serial.begin(115200);
   Serial.println("-!>\tSTART\tSystem");
+  scale.begin(HX711DATA, HX711CLK);
   sensor.type = SENSOR_TYPE;
   sensor.addAutoUintEntry(weight, get_weight_reading);
+  // sensor.addAutoUintEntry(bla, get_blablabla);
+  // sensor.addAutoUintEntry(weight, get_weight_reading);
   uint8_t address = readAddress();
   Serial.print("-->\tSystem.Address\t0x");
   Serial.println(address, HEX);
